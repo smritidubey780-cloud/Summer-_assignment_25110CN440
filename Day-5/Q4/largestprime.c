632621961fd5852469891150f06ddd82dd6543c3
smritidubey780-cloud <smritidubey780@gmail.com>
2026-06-05 22:49:24 +0530

    Day-5 uploaded

---
+++ Day-5/Q4/largestprime.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    long long num;
+    long long largestPrimeFactor = -1;
+
+    printf("Enter a number: ");
+    scanf("%lld", &num);
+
+    while (num % 2 == 0) {
+        largestPrimeFactor = 2;
+        num /= 2;
+    }
+
+    for (long long i = 3; i * i <= num; i += 2) {
+        while (num % i == 0) {
+            largestPrimeFactor = i;
+            num /= i;
+        }
+    }
+
+    if (num > 2) {
+        largestPrimeFactor = num;
+    }
+
+    printf("Largest Prime Factor = %lld\n", largestPrimeFactor);
+
+    return 0;
+}
