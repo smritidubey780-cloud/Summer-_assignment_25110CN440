084e945f3a7da193d84cb0052d3a94ba0744b385
smritidubey780-cloud <smritidubey780@gmail.com>
2026-06-24 20:23:11 +0530

    Day-25 uploaded

---
+++ Day-25/Q2/commonchar.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str1[100], str2[100];
+    int freq[256] = {0};
+    int i;
+
+    printf("Enter first string: ");
+    scanf("%s", str1);
+
+    printf("Enter second string: ");
+    scanf("%s", str2);
+
+    for (i = 0; str1[i] != '\0'; i++) {
+        freq[(unsigned char)str1[i]] = 1;
+    }
+
+    printf("Common characters: ");
+
+    for (i = 0; str2[i] != '\0'; i++) {
+        if (freq[(unsigned char)str2[i]] == 1) {
+            printf("%c ", str2[i]);
+            freq[(unsigned char)str2[i]] = 0; // avoid duplicates
+        }
+    }
+
+    printf("\n");
+
+    return 0;
+}
